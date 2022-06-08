e50917cb9fd903ed23c50c00374a6a94664299f7
Yauri Attamimi <yauri.attamimi@moove.io>
2022-06-08 13:35:51 +0700

    added dtos related to uber payments and trips

--- src/main/java/io/moove/uberdatacomparator/uberapi/dto/PaymentComponent.java
+++ src/main/java/io/moove/uberdatacomparator/uberapi/dto/PaymentComponent.java
@@ -1,2 +1,17 @@
-package io.moove.uberdatacomparator.uberapi.dto;public class PaymentComponent {
+package io.moove.uberdatacomparator.uberapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PaymentComponent {
+
+    @JsonProperty("service_fee")
+    private double serviceFee;
+    private double other;
+    private double toll;
 }
--- src/main/java/io/moove/uberdatacomparator/uberapi/dto/PaymentRecord.java
+++ src/main/java/io/moove/uberdatacomparator/uberapi/dto/PaymentRecord.java
@@ -1,2 +1,30 @@
-package io.moove.uberdatacomparator.uberapi.dto;public class PaymentRecord {
+package io.moove.uberdatacomparator.uberapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PaymentRecord {
+
+    @JsonProperty("payment_id")
+    private String paymentId;
+    @JsonProperty("partner_id")
+    private String partnerId;
+    @JsonProperty("driver_id")
+    private String driverId;
+    private String category;
+    private double amount;
+    @JsonProperty("cash_collected")
+    private double cashCollected;
+    private PaymentComponent breakdown;
+    @JsonProperty("rider_fees")
+    private RiderFeeComponent riderFees;
+    @JsonProperty("trip_id")
+    private String tripId;
+    @JsonProperty("currency_code")
+    private String currencyCode;
+    @JsonProperty("event_time")
+    private long eventTime;
 }
